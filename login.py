0010d35dd9f9699dd6dc2b4d45e01fcf98e141a6
Royston Dsouza <roystond402@gmail.com>
2025-03-15 00:28:39 +0530

    Login page connected with python file with flask

---
+++ login.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template, jsonify
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+
+app = Flask(__name__)  
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '' 
+app.config['MYSQL_DB'] = 'users'
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST']) 
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM users WHERE username = % s AND password = % s', (username, password, ))
+    account = cursor.fetchone()
+    if account:
+        return "0"
+    else:
+        return render_template('login.html') + '<script>alert("Login unsuccessful. Please try again.")</script>'
+if __name__ == '__main__':
+    app.run(debug=True)
